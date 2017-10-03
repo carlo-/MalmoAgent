13262ac885d6fedebd7909bf7a34c32d7768029b
Carlo Rapisarda <rapisarda.carlo@gmail.com>
2017-10-03 13:04:17 +0200

    init

--- Observations.java
+++
@@ -1,8 +0,0 @@
-import javax.xml.bind.annotation.XmlRootElement;
-
-/**
- * Created by Mart on 27.09.2017.
- */
-@XmlRootElement
-public class Observations {
-}
---
+++ src/Observations.java
@@ -0,0 +1,8 @@
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created by Mart on 27.09.2017.
+ */
+@XmlRootElement
+public class Observations {
+}
